0c51493a2f486fe6fe009b4d70a3b39b856ab298
Salim Benfarhat <28115417+salimbenfarhat@users.noreply.github.com>
2025-03-29 08:40:37 +0100

    feat(api): setup axios client and user data fetching methods

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HorizontalNav from "./components/HorizontalNav";
 import VerticalNav from "./components/VerticalNav";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
 function App() {
   const [isScreenSmall, setIsScreenSmall] = useState(false);
-  
-    // Vérifie la taille de l'écran
-    useEffect(() => {
-      const checkScreenSize = () => {
-        const isSmall = window.innerWidth < 1024 || window.innerHeight < 780;
-        setIsScreenSmall(isSmall);
-      };
-  
-      checkScreenSize(); // Vérifie la taille d'écran lors du chargement
-      window.addEventListener('resize', checkScreenSize); // Surveille les changements de taille
-      return () => window.removeEventListener('resize', checkScreenSize);
-    }, []);
-  
-    // Si l'écran est trop petit, affiche un message couvrant toute la page
-    if (isScreenSmall) {
-      return (
-        <div className="screen-too-small">
-          <p>Veuillez utiliser un écran avec une résolution de 1024x780 pixels ou plus.</p>
-        </div>
-      );
-    }
+
+  // Vérifie la taille de l'écran
+  useEffect(() => {
+    const checkScreenSize = () => {
+      const isSmall = window.innerWidth < 1024 || window.innerHeight < 780;
+      setIsScreenSmall(isSmall);
+    };
+
+    checkScreenSize(); // Vérifie la taille d'écran lors du chargement
+    window.addEventListener("resize", checkScreenSize); // Surveille les changements de taille
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  // Si l'écran est trop petit, affiche un message couvrant toute la page
+  if (isScreenSmall) {
+    return (
+      <div className="screen-too-small">
+        <p>
+          Veuillez utiliser un écran avec une résolution de 1024x780 pixels ou
+          plus.
+        </p>
+      </div>
+    );
+  }
 
   return (
-    <div className="app">
-      <header>
-        <HorizontalNav />
-      </header>
-      <main className="main-content">
-        <aside>
-          <VerticalNav />
-        </aside>
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <header>
+          <HorizontalNav />
+        </header>
+        <main className="main-content">
+          <aside>
+            <VerticalNav />
+          </aside>
+          <section className="dashboard-section">
+            <Routes>
+              <Route path="/dashboard/:id" element={<Dashboard />} />
+            </Routes>
+          </section>
+        </main>
+      </div>
+    </Router>
   );
 }
 
